1bede012087bc27362aee53e7e15365e642a5a6f
NutchaDan <nutcha.siriwut@gmail.com>
2022-08-11 08:56:35 +0700

    add word to 5 word

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word1 = "Hello";
+const word2 = "Nutcha";
+const word3 = "Dansiriwut";
+const word4 = "Keyboard";
+const word5 = "Computer";
+
 function App() {
   return (
     <div>
-      <WordCard value={word}/>
+      <WordCard value={word1}/>
+      <WordCard value={word2}/>
+      <WordCard value={word3}/>
+      <WordCard value={word4}/>
+      <WordCard value={word5}/>
     </div>
   );
 }
